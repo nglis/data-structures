a4d68ed1f4392fefc2857e2d5d1f0d66149d2cc6
menmai <nglisic10@gmail.com>
2020-08-10 22:44:51 -0600

    Depth-First Tree Traversal

--- src/BinarySearchTree/BinarySearchTree.java
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -63,4 +63,28 @@ public class BinarySearchTree {
 
         return Integer.max(height(root.left), height(root.right)) + 1;
     }
+
+    public void preOrderTraversal(BSTNode root) {
+        if (root == null) return;
+
+        System.out.println(root.value);
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
+
+    public void inOrderTraversal(BSTNode root) {
+        if (root == null) return;
+
+        inOrderTraversal(root.left);
+        System.out.println(root.value);
+        inOrderTraversal(root.right);
+    }
+
+    public void postOrderTraversal(BSTNode root) {
+        if (root == null) return;
+
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        System.out.println(root.value);
+    }
 }
