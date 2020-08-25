5e751df2591fa3dd827197031e54712987dfd2a7
menmai <nglisic10@gmail.com>
2020-08-24 21:54:45 -0600

    Added notes on graphs

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -1,7 +1,9 @@
 package Graph;
 
 /* Graphs can be represented with a set of vertices and edges
- *
+ * Graphs can be directed or undirected, and can contain self loops and multiedges.
+ * The maximum number of edges in a simple directed graph is n(n-1) where n = |# of Vertices|
+ * The maximum of edges in a simple undirected graph is (n/2)(n-1)
  */
 
 public class Graph {
