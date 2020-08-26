62bbb73500bc5e3e6d02c96efd569481a7d974fb
menmai <nglisic10@gmail.com>
2020-08-25 22:37:31 -0600

    Implemented vertex/edge lists for graphs

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -6,5 +6,37 @@ package Graph;
  * The maximum of edges in a simple undirected graph is (n/2)(n-1)
  */
 
+import java.util.ArrayList;
+
+class Edge {
+    // startVertex & endVertex can be points to the vertex objects OR
+    // even better - they can refer to the INDEX of the object to which they refer
+    int startVertex;
+    int endVertex;
+    // Edges can also have weights
+    int weight;
+
+    // In the undirected case, startVertex and endVertex can be used interchangeably
+    Edge(int startVertex, int endVertex, int weight) {
+        this.startVertex = startVertex;
+        this.endVertex = endVertex;
+        this.weight = weight;
+    }
+}
+
+class Vertex {
+    String vertex;
+
+    Vertex(String vertex) {
+        this.vertex = vertex;
+    }
+
+    public String toString() {
+        return vertex;
+    }
+}
+
 public class Graph {
+    ArrayList vertices = new ArrayList<Vertex>();
+    ArrayList edges = new ArrayList<Edge>();
 }
