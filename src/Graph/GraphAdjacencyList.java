c5dad2f2b8a2fc26a000ed257035c40ce3d793ff
menmai <nglisic10@gmail.com>
2020-08-27 23:08:00 -0600

    Added skeleton for Graph Adjacency List

---
+++ src/Graph/GraphAdjacencyList.java
@@ -0,0 +1,12 @@
+package Graph;
+
+public class GraphAdjacencyList {
+    /* A graph adjacency list is used to solve the space complexity issue
+     * of a graph adjacency matrix, as well as the time complexity issue
+     * of a graph edge list.
+     */
+
+    /* Implementation could use a single array of 1D arrays with pointers
+     * to endpoint nodes.
+     */
+}
