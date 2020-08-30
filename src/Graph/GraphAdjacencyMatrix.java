a8c8cfbac924135fc723b7175c59902923c44172
menmai <nglisic10@gmail.com>
2020-08-29 23:59:06 -0600

    Added skeleton for Graph Adjacency List

--- src/Graph/GraphAdjacencyMatrix.java
+++ src/Graph/GraphAdjacencyMatrix.java
@@ -6,9 +6,9 @@ public class GraphAdjacencyMatrix {
      * O(v^2) where v is the number of vertices.
      */
 
-    /* An implementation could use an array of 1D arrays, where each value in the 1D array represents the
+    /* An implementation could use an array of 1D arrays/linked lists, where each value in the 1D array represents the
      * Edge between node pairs. An arbitrary value like (0 or ∞) can be used to represented no edge between
      * two node. The indices of the 1D arrays within the single array, and the indices of the outer array
-     * can be used as a representation of the node (endpoints), and the node (startpoint), respectively.
+     * can be used as a representation of the node (endpoints), and the node (startpoint), respectively.6+
      */
 }
